e01bf495e683dfb834607848b93e84517fb28ca7
Shorif <shorifahamed571@gmail.com>
2023-11-25 03:01:02 +0600

    User post operaton created

--- index.js
+++ index.js
@@ -26,12 +26,22 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const userCollection = client.db('bloodDonation').collection("users")
+
+    // user api
+    app.post('/users', async(req, res)=>{
+        const user = req.body;
+        const result = await userCollection.insertOne(user);
+        res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
