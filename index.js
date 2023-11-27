5ef96777841a3366edee155d327268fec99b52c4
Shorif <shorifahamed571@gmail.com>
2023-11-27 09:46:36 +0600

    Blog delete opreation created

--- index.js
+++ index.js
@@ -145,6 +145,14 @@ async function run() {
       res.send(result)
     })
 
+    app.delete('/blogs/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) }
+      const result = await blogsCollection.deleteOne(query);
+      res.send(result);
+    })
+
 
 
 
